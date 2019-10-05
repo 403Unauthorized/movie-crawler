7ab55aa417dc1b8ff0a94d4afaf87482be85e672
leiyongqi <2540356035@qq.com>
2019-10-05 11:21:44 +0900

    添加解决跨域问题的拦截器

---
+++ src/main/java/com/raja/crawler/config/WebAppConfig.java
@@ -0,0 +1,22 @@
+package com.raja.crawler.config;
+
+import com.raja.crawler.interceptor.AllowCorsInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfig implements WebMvcConfigurer {
+
+    private final AllowCorsInterceptor allowCorsInterceptor;
+
+    public WebAppConfig(AllowCorsInterceptor allowCorsInterceptor) {
+        this.allowCorsInterceptor = allowCorsInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(allowCorsInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/raja/crawler/interceptor/AllowCorsInterceptor.java
@@ -0,0 +1,36 @@
+package com.raja.crawler.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AllowCorsInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String origin = request.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", StringUtils.isEmpty(origin) ? "*" : origin);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+        response.setHeader("Access-Control-Max-Age", "0");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("XDomainRequestAllowed","1");
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
